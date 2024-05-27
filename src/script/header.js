a9557d497d55be82e44c3f7e07c5e061b195c21e
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-05-27 13:32:44 +0200

    updates all pages

--- src/script/header.js
+++ src/script/header.js
@@ -3,8 +3,11 @@ const nav = document.createElement("nav");
 nav.classList.add("nav", "nav-style", "sticky-top"); // les classes ici
 nav.innerHTML = `
     <a class="nav-item-style" href="../../index.html">Home</a>
-    <a class="nav-item-style" href="../pages/project.html">Projets </a>
-    <a class="nav-item-style" href="../pages/contact.html">Moi</a>
+    
+    <a class="nav-item-style" href="../pages/about-me.html">A propos</a>
 `;
 
 document.body.prepend(nav); // prepend pour placer l'élément en début du body
+
+// liens réunissant tous mes projets à ajouter plus tard
+// <a class="nav-item-style" href="../pages/project.html">Projets </a>
